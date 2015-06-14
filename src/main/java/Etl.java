dd19491438bed7fad3d27dfe041de2a714acae20
Kim D Barnes <kimdbarnes@gmail.com>
2015-06-13 20:39:35 -0600

    First draft, working

--- src/main/java/Etl.java
+++ src/main/java/Etl.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Etl {
-   Object transform(Object thing) {
-      return null;
+   Object transform(Map<Integer, List<String>> oldValues) {
+      Map<String, Integer> newValues = new HashMap();
+
+      for (Integer value : oldValues.keySet()) {
+         for (String character : oldValues.get(value)) {
+            newValues.put(character.toLowerCase(), value);
+         }
+      }
+
+      return newValues;
    }
 }
